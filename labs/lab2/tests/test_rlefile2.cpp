fceb89d410c345b76049eeac6ee20801fcb79c0d
Dasha <difilatova070907@gmail.com>
2026-04-25 22:22:59 +0300

    Добавлена лабораторная работа lab2 со сделанным TODO

---
+++ labs/lab2/tests/test_rlefile2.cpp
@@ -0,0 +1,63 @@
+#include "../src/rlefile2.hpp"
+#include "../src/basefile.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+// Тест для RleFile2 (композиция) со случайными данными
+bool test_rlefile2_random() {
+    std::cout << "\n Тест RleFile2 (композиция) со случайными данными \n" << std::endl;
+    
+    const size_t DATA_SIZE = 50000;
+    unsigned char* original = new unsigned char[DATA_SIZE];
+    
+    srand(time(nullptr));
+    for (size_t i = 0; i < DATA_SIZE; i++) {
+        original[i] = rand() % 256;
+    }
+    
+    // Запись
+    {
+        RleFile2 f(new BaseFile("data/test_rlefile2_random.rle", "w"));
+        size_t total_written = 0;
+        for (size_t i = 0; i < DATA_SIZE; i += 256) {
+            size_t chunk_size = (i + 256 < DATA_SIZE) ? 256 : (DATA_SIZE - i);
+            total_written += f.write(original + i, chunk_size);
+        }
+        if (total_written != DATA_SIZE) {
+            std::cout << "Ошибка: записано не все!" << std::endl;
+            delete[] original;
+            return false;
+        }
+        std::cout << "Записано " << total_written << " байт" << std::endl;
+    }
+    
+    // Чтение
+    unsigned char* read_data = new unsigned char[DATA_SIZE];
+    size_t total_read = 0;
+    {
+        RleFile2 reader(new BaseFile("data/test_rlefile2_random.rle", "r"));
+        while (total_read < DATA_SIZE) {
+            size_t chunk_size = (total_read + 256 < DATA_SIZE) ? 256 : (DATA_SIZE - total_read);
+            total_read += reader.read(read_data + total_read, chunk_size);
+        }
+        std::cout << "Прочитано " << total_read << " байт" << std::endl;
+    }
+    
+    // Сравнение
+    bool ok = (total_read == DATA_SIZE);
+    for (size_t i = 0; i < DATA_SIZE && ok; i++) {
+        if (original[i] != read_data[i]) ok = false;
+    }
+    
+    delete[] original;
+    delete[] read_data;
+    
+    std::cout << (ok ? "Тест RleFile2 пройден!" : "Тест RleFile2 НЕ пройден!") << std::endl;
+    return ok;
+}
+
+int main() {
+    return test_rlefile2_random() ? 0 : 1;
+}
